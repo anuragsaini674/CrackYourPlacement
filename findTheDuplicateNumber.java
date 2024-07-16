d2c3e4ba46a7c66d9df73b91c43762ee25b3dde8
Saini <Anurag.Saini@motherson.com>
2024-07-16 09:14:07 +0530

    findTheDuplicateNumber

---
+++ findTheDuplicateNumber.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class findTheDuplicateNumber{
+
+    public static int calculate(int arr[]){
+
+        int slow=0, fast=0;
+
+        do{
+            slow = arr[slow];
+            fast = arr[arr[fast]];
+        }while(slow!=fast);
+
+        slow=0;
+        while(slow!=fast){
+            slow = arr[slow];
+            fast = arr[fast];
+        }
+
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        size--;
+        while(size>=0){
+            arr[size] = sc.nextInt();
+            size--;
+        }
+
+        System.out.println("Answer: "+calculate(arr));
+    }
+}
